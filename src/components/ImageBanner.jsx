84f625fe16b9032013b321183e91494cc60a8a62
plebbitor <mottoes_recap0u@icloud.com>
2023-03-03 14:44:34 +0100

    fixed ImageBanner rendering

--- src/components/ImageBanner.jsx
+++ src/components/ImageBanner.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const ImageBanner = () => {
   const [currentImage, setCurrentImage] = useState(1);
+  const location = useLocation();
 
   useEffect(() => {
     setCurrentImage(Math.floor(Math.random() * 13) + 1);
-  }, []);
+  }, [location.key]);
 
   return (
     <img id="banner-img" src={`/assets/banners/banner-${currentImage}.jpg`} alt="banner" />
