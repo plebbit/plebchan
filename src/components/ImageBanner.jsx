279cb8c4752b3ca1b83b87920a6d0b4486504596
plebbitor <mottoes_recap0u@icloud.com>
2023-04-19 14:16:04 +0200

    fix imagebanner glitch

--- src/components/ImageBanner.jsx
+++ src/components/ImageBanner.jsx
@@ -2,32 +2,36 @@ import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ImageBanner = () => {
-  const [currentImage, setCurrentImage] = useState(1);
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [currentImage, setCurrentImage] = useState(null);
   const location = useLocation();
 
   useEffect(() => {
-    const countImages = async () => {
+    let isMounted = true;
+
+    const loadRandomImage = async () => {
       const images = await importAll(require.context('../../public/assets/banners', false, /\.(png|jpe?g|svg)$/));
-      setCurrentImage(Math.floor(Math.random() * images.length) + 1);
+      const randomImage = Math.floor(Math.random() * images.length) + 1;
+
+      const img = new Image();
+      img.src = `${process.env.PUBLIC_URL}/assets/banners/banner-${randomImage}.jpg`;
+
+      img.onload = () => {
+        if (isMounted) {
+          setCurrentImage(randomImage);
+        }
+      };
     };
 
-    setIsLoaded(false);
-    countImages();
+    loadRandomImage();
+
+    return () => {
+      isMounted = false;
+    };
   }, [location.key]);
-
-  useEffect(() => {
-    setIsLoaded(false);
-    const img = new Image();
-    img.src = `${process.env.PUBLIC_URL}/assets/banners/banner-${currentImage}.jpg`;
-    img.onload = () => {
-      setIsLoaded(true);
-    };
-  }, [currentImage]);
 
   return (
     <>
-      {isLoaded && <img id="banner-img" src={`${process.env.PUBLIC_URL}/assets/banners/banner-${currentImage}.jpg`} alt="banner" />}
+      {currentImage && <img id="banner-img" src={`${process.env.PUBLIC_URL}/assets/banners/banner-${currentImage}.jpg`} alt="banner" />}
     </>
   );
 };
