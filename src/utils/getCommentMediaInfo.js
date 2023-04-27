428b335cfcc156214e73a0279ae2b9c2bbb2280d
Tom <mottoes_recap0u@icloud.com>
2023-04-27 11:34:25 +0200

    fix getCommentMediaInfo

--- src/utils/getCommentMediaInfo.js
+++ src/utils/getCommentMediaInfo.js
@@ -4,12 +4,7 @@ const getCommentMediaInfo = (comment) => {
   if (!comment?.thumbnailUrl && !comment?.link) {
     return;
   }
-  // if (comment?.thumbnailUrl) {
-  //   return {
-  //     url: comment.link,
-  //     type: 'webpage'
-  //   };
-  // }
+
   if (comment?.link) {
     try {
       new URL(comment.link);
@@ -23,23 +18,30 @@ const getCommentMediaInfo = (comment) => {
     if (mime?.startsWith('image')) {
       return {
         url: comment.link,
-        type: 'image'
+        type: 'image',
       };
     }
-  
+
     if (mime?.startsWith('video')) {
       return {
         url: comment.link,
-        type: 'video'
+        type: 'video',
       };
     }
 
     if (mime?.startsWith('audio')) {
       return {
         url: comment.link,
-        type: 'audio'
+        type: 'audio',
       };
     }
+  }
+
+  if (comment?.thumbnailUrl !== comment?.link) {
+    return {
+      url: comment.link,
+      type: 'webpage',
+    };
   }
 };
 
