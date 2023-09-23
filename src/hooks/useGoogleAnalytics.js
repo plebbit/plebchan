93eb4d78cf424f0ddd816273c7875d8a104770ee
plebeius.eth <117766773+plebeius-eth@users.noreply.github.com>
2023-09-23 21:15:03 +0200

    Update useGoogleAnalytics.js

--- src/hooks/useGoogleAnalytics.js
+++ src/hooks/useGoogleAnalytics.js
@@ -6,15 +6,20 @@ const GA_MEASUREMENT_ID = "G-2M2VYEFL7B";
 const useGoogleAnalytics = () => {
   const location = useLocation();
 
-  useEffect(() => {
-    if (typeof window.gtag === 'function') {
-      const fullPath = location.pathname + location.search + location.hash;
-      window.gtag('config', GA_MEASUREMENT_ID, {
-        'page_path': fullPath,
-      });
-      window.gtag('event', 'page_view', {'page_path': fullPath});
-    }
-  }, [location]);
+useEffect(() => {
+  if (typeof window.gtag === 'function') {
+    const fullPath = location.pathname + location.search + location.hash;
+    window.gtag('config', GA_MEASUREMENT_ID, {
+      'page_path': fullPath,
+    });
+    window.gtag('event', 'page_view', {
+    'page_path': fullPath,
+    'page_location': window.location.href
+  });
+
+  }
+}, [location]);
+
 };
 
-export default useGoogleAnalytics;
+export default useGoogleAnalytics;pat
