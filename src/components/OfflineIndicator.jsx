4eba8e7530516a798396696418e013251740481d
plebeius.eth <mottoes_recap0u@icloud.com>
2023-10-04 21:17:00 +0200

    fix(offline indicator): check for online status every 30 minutes instead of 20

--- src/components/OfflineIndicator.jsx
+++ src/components/OfflineIndicator.jsx
@@ -3,7 +3,7 @@ import { useSubplebbit } from '@plebbit/plebbit-react-hooks';
 
 const OfflineIndicator = ({ address, className, tooltipPlace }) => {
   const subplebbit = useSubplebbit({ subplebbitAddress: address });
-  const isOnline = subplebbit.updatedAt > Date.now() / 1000 - 60 * 20;
+  const isOnline = subplebbit.updatedAt > Date.now() / 1000 - 60 * 30;
 
   return (
     <>
