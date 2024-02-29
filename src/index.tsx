a64de010578d602c5ec4961f15ee12cffbb39117
plebeius.eth <mottoes_recap0u@icloud.com>
2024-02-29 11:41:57 +0100

    init translations

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 import { HashRouter as Router } from 'react-router-dom';
+import './lib/init-translations';
 import './index.css';
 import './themes.css';
 import { App as CapacitorApp } from '@capacitor/app';
