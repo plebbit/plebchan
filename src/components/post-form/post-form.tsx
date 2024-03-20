5ef7f0a77d4f2063744ab42de8b158d37ef93303
plebeius.eth <mottoes_recap0u@icloud.com>
2024-03-20 13:08:08 +0100

    chore(translations): start a new thread

--- src/components/post-form/post-form.tsx
+++ src/components/post-form/post-form.tsx
@@ -1,9 +1,12 @@
 import styles from './post-form.module.css';
+import { useTranslation } from 'react-i18next';
 
 const PostForm = () => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.togglePostForm}>
-      [<span>Start a New Thread</span>]
+      [<span>{t('start_new_thread')}</span>]
     </div>
   );
 };
