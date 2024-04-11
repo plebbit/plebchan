79b7b0193b909ed3d373797b74d361ad0bd6db58
plebeius.eth <mottoes_recap0u@icloud.com>
2024-04-11 16:52:20 +0200

    add new banners

--- src/components/board-banner/board-banner.tsx
+++ src/components/board-banner/board-banner.tsx
@@ -3,7 +3,7 @@ import { useSubplebbit } from '@plebbit/plebbit-react-hooks';
 import { useState } from 'react';
 import styles from './board-banner.module.css';
 
-const totalBanners = 46;
+const totalBanners = 51;
 
 const ImageBanner = () => {
   const [imagePath] = useState(() => {
