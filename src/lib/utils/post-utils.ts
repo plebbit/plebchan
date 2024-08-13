77980ee0c64a413f7582863487c23e7fc6b5ac9d
Tom (plebeius.eth) <tom@plebbit.com>
2024-08-13 21:40:34 +0200

    fix undefined

--- src/lib/utils/post-utils.ts
+++ src/lib/utils/post-utils.ts
@@ -1,4 +1,8 @@
 export function hashStringToColor(str: string): string {
+  if (!str) {
+    return '';
+  }
+
   let hash = 0;
   for (let i = 0; i < str.length; i++) {
     hash = str.charCodeAt(i) + ((hash << 5) - hash);
