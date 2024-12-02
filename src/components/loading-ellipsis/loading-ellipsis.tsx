09505a62313fcf26de4a1e84d7f705eee714bd33
Tom (plebeius.eth) <tom@plebbit.com>
2024-12-02 10:33:17 +0100

    fix(ellipsis animation): dots could appear cut off and cause displacement changing width of string

--- src/components/loading-ellipsis/loading-ellipsis.tsx
+++ src/components/loading-ellipsis/loading-ellipsis.tsx
@@ -5,7 +5,20 @@ interface LoadingEllipsisProps {
 }
 
 const LoadingEllipsis = ({ string }: LoadingEllipsisProps) => {
-  return <span className={styles.ellipsis}>{string}</span>;
+  const words = string.split(' ');
+  const lastWord = words.pop();
+  const restOfString = words.join(' ');
+
+  return (
+    <span>
+      {restOfString}
+      {restOfString && ' '}
+      <span className={styles.nowrap}>
+        {lastWord}
+        <span className={styles.ellipsis} />
+      </span>
+    </span>
+  );
 };
 
 export default LoadingEllipsis;
